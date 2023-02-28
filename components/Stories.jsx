53eb5ed11a78d39d2846f75d4559e97b2b225f0d
DeepGandhi07 <deepgandhi9887@gmail.com>
2023-02-28 15:58:23 +0530

    Commit

--- components/Stories.jsx
+++ components/Stories.jsx
@@ -1,7 +1,14 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import * as faker from "@faker-js/faker";
 const Stories = () => {
-  return <div>Stories</div>;
+  useEffect(() => {
+    const suggestion = [...Array(20)].map((_, i) => ({
+      ...faker.helpers?.contextualCard(),
+      id: i,
+    }));
+  }, []);
+
+  return <div>{/* Story */}</div>;
 };
 
 export default Stories;
