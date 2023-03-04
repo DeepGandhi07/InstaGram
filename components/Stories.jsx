60a12db2ade8a554f8cc890b7804088a4bd06254
DeepGandhi07 <deepgandhi9887@gmail.com>
2023-03-04 20:23:07 +0530

    Commit

--- components/Stories.jsx
+++ components/Stories.jsx
@@ -1,14 +1,33 @@
-import React, { useEffect } from "react";
-import * as faker from "@faker-js/faker";
+import React, { useEffect, useState } from "react";
+// import * as faker from "@faker-js/faker";
+import { faker } from "@faker-js/faker";
+import Story from "./Story";
 const Stories = () => {
+  const [suggestions, setSuggestions] = useState([]);
   useEffect(() => {
-    const suggestion = [...Array(20)].map((_, i) => ({
-      ...faker.helpers?.contextualCard(),
-      id: i,
+    const suggestions = [...Array(20)].map((_, i) => ({
+      userId: faker.datatype.uuid(),
+      username: faker.internet.userName(),
+      email: faker.internet.email(),
+      avatar: faker.image.avatar(),
+      password: faker.internet.password(),
+      birthdate: faker.date.birthdate(),
+      registeredAt: faker.date.past(),
     }));
+    setSuggestions(suggestions);
   }, []);
 
-  return <div>{/* Story */}</div>;
+  return (
+    <div>
+      {suggestions.map((profile) => (
+        <Story key={}/>
+      ))}
+      {/* Story */}
+      {/* Story */}
+      {/* Story */}
+      {/* Story */}
+    </div>
+  );
 };
 
 export default Stories;
