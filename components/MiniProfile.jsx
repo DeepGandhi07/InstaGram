690b681d56fd2a55249a1d73df55ebd1c01c3ac2
DeepGandhi07 <deepgandhi9887@gmail.com>
2023-03-05 13:27:27 +0530

    Commit

--- components/MiniProfile.jsx
+++ components/MiniProfile.jsx
@@ -10,11 +10,11 @@ const MiniProfile = () => {
         alt="Profile Image"
         className=" h-16 w-16 rounded-full p-[2px] "
       />
-      <div>
-        <h2>deepgandhi.08</h2>
-        <h3>Welcome to Instagram</h3>
+      <div className="flex-1 mx-4">
+        <h2 className="font-bold">deepgandhi.08</h2>
+        <h3 className="text-sm text-gray-400">Welcome to Instagram</h3>
       </div>
-      <button>Sign Out</button>
+      <button className="text-blue-400 text-sm font-semibold">Sign Out</button>
     </div>
   );
 };
