55c199b6ac7bf489fd2bd385c1b848a411fd28e6
DeepGandhi07 <deepgandhi9887@gmail.com>
2023-03-06 01:35:05 +0530

    Commit

--- components/Stories.jsx
+++ components/Stories.jsx
@@ -2,7 +2,9 @@ import React, { useEffect, useState } from "react";
 // import * as faker from "@faker-js/faker";
 import { faker } from "@faker-js/faker";
 import Story from "./Story";
+import { useSession } from "next-auth/react";
 const Stories = () => {
+  const { data: session } = useSession();
   const [suggestions, setSuggestions] = useState([]);
   useEffect(() => {
     const suggestions = [...Array(20)].map((_, i) => ({
@@ -20,6 +22,10 @@ const Stories = () => {
 
   return (
     <div className="flex space-x-2 p-6 bg-white mt-8 border-gray-200 border rounded-sm overflow-x-scroll scrollbar-thin scrollbar-thumb-black">
+      {session && (
+        <Story img={session?.user?.image} username={session?.user?.username} />
+      )}
+
       {suggestions.map((profile) => (
         <Story
           key={profile.userId}
