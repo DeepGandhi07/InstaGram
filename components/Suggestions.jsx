fbf24756db11ba56f2e4e2137b39c01d2bf84829
DeepGandhi07 <deepgandhi9887@gmail.com>
2023-03-05 13:27:03 +0530

    Commit

--- components/Suggestions.jsx
+++ components/Suggestions.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { faker } from "@faker-js/faker";
 
 const Suggestions = () => {
-  return <div>Suggestions</div>;
+  const [suggestions, setSuggestions] = useState([]);
+  useEffect(() => {
+    const suggestions = [...Array(5)].map((_, i) => ({
+      userId: faker.datatype.uuid(),
+      username: faker.internet.userName(),
+      email: faker.internet.email(),
+      avatar: faker.image.avatar(),
+      password: faker.internet.password(),
+      birthdate: faker.date.birthdate(),
+      registeredAt: faker.date.past(),
+    }));
+    setSuggestions(suggestions);
+  }, []);
+  return (
+    <div className="mt-4 ml-10">
+      <div className="flex justify-between text-sm mb-5">
+        <h3 className="text-sm font-bold text-gray-400">Suggestions for you</h3>
+        <button className="text-gray-600 font-semibold">Seel All</button>
+      </div>
+      {suggestions.map((profile) => (
+        <div
+          key={profile.id}
+          className="flex items-center justify-between mt-3"
+        >
+          <img
+            className="w-10 h-10 rounded-full border p-[2px]"
+            src={profile.avatar}
+            alt=""
+          />
+          <div>
+            <h2></h2>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Suggestions;
