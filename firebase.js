a813569cb80a2162b6f3e41d72c5706ce732c9f0
DeepGandhi07 <deepgandhi9887@gmail.com>
2023-03-05 14:55:48 +0530

    Commit

---
+++ firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyDj1VeUJ55xZnCw5PRKcDs7Sxec8v-L_QY",
+  authDomain: "instagram-a762b.firebaseapp.com",
+  projectId: "instagram-a762b",
+  storageBucket: "instagram-a762b.appspot.com",
+  messagingSenderId: "798807471924",
+  appId: "1:798807471924:web:1ecbd2bc7eacca247f9761",
+  measurementId: "G-W3Z6MBGS6P",
+};
+
+const app = initializeApp(firebaseConfig);
