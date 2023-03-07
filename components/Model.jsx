a8f6dbd98d2010381f8944049f508115e73ff7f5
DeepGandhi07 <deepgandhi9887@gmail.com>
2023-03-07 17:57:54 +0530

    Commit

--- components/Model.jsx
+++ components/Model.jsx
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { useRecoilState } from "recoil";
 import { modalState } from "../atoms/modalAtom";
 import { Dialog, Transition } from "@headlessui/react";
 import { Fragment } from "react";
+import { CameraIcon } from "@heroicons/react/24/outline";
 const Model = () => {
   const [open, setOpen] = useRecoilState(modalState);
+  const [loading, setLoading] = useState(false);
+  const filePickerRef = useRef(null);
+  const captionRef = useRef(null);
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const uploadPost = async () => {
+    if (loading) return;
+
+    setLoading(true);
+    // 1) Create a post and add to FireStore 'posts' collection
+    // 2) Get the post ID for the newly created post
+    // 3) upload the image to firebase storage with the post ID
+    // 4) get a download URL from fb storage and upload to original Post
+  };
+  const addImageToPost = (e) => {
+    const render = new FileReader();
+    if (e.target.files[0]) {
+      render.readAsDataURL(e.target.files[0]);
+    }
+    render.onload = (renderEvent) => {
+      setSelectedFile(renderEvent.target.result);
+    };
+  };
   return (
     <Transition.Root show={open} as={Fragment}>
       <Dialog
@@ -42,6 +66,24 @@ const Model = () => {
           >
             <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-0 sm:align-middle sm:max-w-sm sm:w-full sm:p-6 ">
               <div>
+                {selectedFile ? (
+                  <img
+                    src={selectedFile}
+                    className="w-full object-contain cursor-pointer"
+                    alt=""
+                    onClick={() => setSelectedFile(null)}
+                  />
+                ) : (
+                  <div
+                    onClick={() => filePickerRef.current.click()}
+                    className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-red-100 cursor-pointer"
+                  >
+                    <CameraIcon
+                      className="h-6 w-6 text-red-600"
+                      aria-hidden="true"
+                    />
+                  </div>
+                )}
                 <div>
                   <div className="mt-3 text-center sm:mt-5">
                     <Dialog.Title
@@ -52,17 +94,17 @@ const Model = () => {
                     </Dialog.Title>
                     <div>
                       <input
-                        // ref={filePickerRef}
+                        ref={filePickerRef}
                         type="file"
                         hidden
-                        // onChange={addImageToPost}
+                        onChange={addImageToPost}
                       />
                     </div>
                     <div className="mt-2">
                       <input
                         className="border-none focus:ring-0 w-full text-center "
                         type="text"
-                        // ref={captionsRef}
+                        ref={captionRef}
                         placeholder="Please enter a caption..."
                       />
                     </div>
